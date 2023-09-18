3be19a1a2a8bb2fd1543274987151cd025f32a13
FinleyCox <111890705+FinleyCox@users.noreply.github.com>
2023-09-18 12:25:04 +0900

    main functions

---
+++ resources/js/Pages/Post.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Konichiwa" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Post</h2>
+        </template>
+
+        <!-- CARDS FOR Post -->
+
+
+
+    </AuthenticatedLayout>
+    
+</template>
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/post', function () {
+    return Inertia::render('Post');
+})->middleware(['auth', 'verified'])->name('post');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
